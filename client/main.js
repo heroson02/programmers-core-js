bf31e4e3c0ff56ad0cee6fe5023fbf3cc4358d6e
heroson02 <heroson0925@gmail.com>
2025-06-17 15:51:26 +0900

    RandomDice MAKE except initialize function

--- client/main.js
+++ client/main.js
@@ -1,3 +1,4 @@
+import { diceAnimation } from './lib/animation/cube.js';
 import {
   clearContents,
   getNode,
@@ -6,38 +7,61 @@ import {
   copy,
   isNumber,
   isUndefined,
+  attr,
 } from './lib/index.js';
 
-let submitBtn = getNode('#submit');
-let nameField = getNode('#nameField');
-let result = getNode('.result');
+import {} from './lib/index.js';
 
-export default function jujeobData(nickName) {
-  return [
-    `대한민국 ${nickName} 보유국`,
-    `나 몰랐는데 ${nickName} 좋아하네`,
-    `${nickName} MBTI 검사하면 Cute나온다며`,
-    `${nickName}? 솔직히 거품 아닌가요? 언빌리 '버블'`,
-    `${nickName} 사진 보다가 심장 녹을까봐 냉동실에서 사진 보는 중이에요`,
-    `${nickName} 넌 뭐랄까.. 마치 베를린 같아. 왜냐하면 치명적인 독일 수도.`,
-    `${nickName} 그거 기억나요? 사람들 한테 ${nickName} 좋아하는 사람 손 접어 했더니 지구가 반으로 접힌거\n그거 겨우겨우 되돌렸잖아요 나 그 때 내 눈 앞에 브라질 있어서 깜짝 놀랐잖아`,
-    `${nickName}씨 예민한 질문일 수 도 있지만 너무 궁금해서 물어봅니다.\n${nickName}씨는 천국에서 쫓겨난 건가요 자발적으로 내려온 건가요\n아님 추락 사고 였나요?`,
-    `짜장면한테 ${nickName} 음악을 들려 줬더니 짜장면이 저절로 비벼졌습니다.`,
-  ];
+const [diceRoll, record, initialize] = document.querySelectorAll(
+  '.buttonGroup button'
+);
+const recordListWrapper = getNode('.recordListWrapper');
+
+
+function handleRollingDice(){
+	let isClicked = false;
+	let id;
+	return (() => {
+		if (!isClicked)
+		{
+			id = setInterval(diceAnimation, 100);
+			console.log('on');
+			record.disabled = true;
+			initialize.disabled = true;
+		}
+		else
+		{
+			clearInterval(id);
+			console.log('off');
+			record.disabled = false;
+			initialize.disabled = false;
+		}
+		isClicked = !isClicked;
+	})
 }
 
-submitBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  if (!nameField.value || nameField.value.replaceAll(' ', '') === '') {
-    console.error('이름을 입력해주세요');
-    return;
-  }
-  if (!isNaN(nameField.value)) {
-    console.error('숫자만 입력할 수 없습니다.');
-    return;
-  }
-  result.textContent = '';
-  let data = jujeobData(nameField.value)[getRandom(9)];
-  insertLast(result, data);
-  copy(data);
-});
+let recordList = document.querySelector('tbody');
+console.log(recordList);
+
+let count = 0;
+let total = 0;
+
+function handleRecord(){
+	const diceNumber =  +attr('#cube', 'dice');
+	recordListWrapper.hidden = false;
+	console.log(diceNumber);
+	const template = `<tr>
+            <td>${++count}</td>
+            <td>${diceNumber}</td>
+            <td>${total += diceNumber}</td>
+          </tr>`;
+	insertLast('tbody', template);
+}
+
+function handleReset(){
+
+}
+
+
+diceRoll.addEventListener('click', handleRollingDice());
+record.addEventListener('click', handleRecord);
